d8d2bd53c9727e4fc861fc069adb5cc438ca03f1
Vlad Sheiko <sheiko.vlad@gmail.com>
2025-07-23 18:31:47 +0300

    mb working front test

--- front/dest/handlesmalmanga.js
+++ front/dest/handlesmalmanga.js
@@ -1,29 +1,32 @@
-console.log('handlesmalmanga.js loaded');
 const api = 'http://localhost:5000';
-document.querySelector('#app').innerHTML =
-    `
-<h1>Library Manga</h1>
-<button id="random" type="button" onclick="getListManga()">RaNdOm</button>
-<h2 id="greeting"></h2>
-<div id="manga-list"></div>`;
-getListManga();
 function getListManga() {
-    fetch(api + '/MangaList/random')
+    fetch(api + '/RandomManga/random?count=8')
         .then(response => response.json())
         .then(data => {
-        const appElem = document.querySelector('#app');
+        const appElem = document.querySelector('#manga-list');
         if (appElem) {
             let mangaList = data;
             appElem.innerHTML = mangaList.map(manga => `
-            <div class="manga">
-              <h3>${manga.title}</h3>
-              <img src="${manga.coverImage}" alt="${manga.title} cover">
-              <p>Status: ${manga.status}</p>
-              <p>Chapters: ${manga.chapters}</p>
-            </div>
-          `).join('');
+          <div class="manga">
+            <h3>${manga.title}</h3>
+            <img src="${manga.coverImage}" alt="${manga.title} cover">
+            <img src="data:image/jpeg;base64,https://manga.in.ua/uploads/posts/2025-06/1751094153_poster_112.png" alt="${123123} cover">
+            
+            <p>Status: ${manga.status}</p>
+            <p>Chapters: ${manga.chapters}</p>
+          </div>
+        `).join('');
         }
     });
 }
 ;
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#app').innerHTML =
+        `
+      <h1>Library Manga</h1>
+      <button id="random" type="button">random</button>
+      <div id="manga-list"></div>
+    `;
+    document.querySelector('#random').addEventListener('click', getListManga);
+});
 export {};
--- front/src/handlesmalmanga.ts
+++ front/src/handlesmalmanga.ts
@@ -1,34 +1,37 @@
-console.log('handlesmalmanga.js loaded');
-import './style.css'
 import type { IMangaTitleShort } from './models/IMangaTitleShort';
 
 const api = 'http://localhost:5000';
+
+
+
+function getListManga() {
+  fetch(api + '/RandomManga/random?count=8')
+    .then(response => response.json())
+    .then(data => {
+      const appElem = document.querySelector('#manga-list');
+      if (appElem) {
+        let mangaList = data as IMangaTitleShort[];
+        appElem.innerHTML = mangaList.map(manga => `
+          <div class="manga">
+            <h3>${manga.title}</h3>
+            <img src="${manga.coverImage}" alt="${manga.title} cover">
+            <img src="data:image/jpeg;base64,https://manga.in.ua/uploads/posts/2025-06/1751094153_poster_112.png" alt="${123123} cover">
+            
+            <p>Status: ${manga.status}</p>
+            <p>Chapters: ${manga.chapters}</p>
+          </div>
+        `).join('');
+      }
+    });
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector<HTMLDivElement>('#app')!.innerHTML =
     `
-<h1>Library Manga</h1>
-<button id="random" type="button" onclick="getListManga()">RaNdOm</button>
-<h2 id="greeting"></h2>
-<div id="manga-list"></div>`
+      <h1>Library Manga</h1>
+      <button id="random" type="button">random</button>
+      <div id="manga-list"></div>
+    `;
 
-  getListManga();
+  document.querySelector('#random')!.addEventListener('click', getListManga);
 });
-
-function getListManga() {
-  fetch(api + '/MangaList/random')
-    .then(response => response.json())
-    .then(data => {
-        const appElem = document.querySelector('#app');
-        if (appElem) {
-            let mangaList = data as IMangaTitleShort[];
-          appElem.innerHTML = mangaList.map(manga => `
-            <div class="manga">
-              <h3>${manga.title}</h3>
-              <img src="${manga.coverImage}" alt="${manga.title} cover">
-              <p>Status: ${manga.status}</p>
-              <p>Chapters: ${manga.chapters}</p>
-            </div>
-          `).join('');
-        }
-    });
-};
